14b359aa3cef9839c53ea3f7195e17dc9a7820e8
Marshall Vogt <vogt_marshall@yahoo.com>
2023-10-19 16:27:29 +0000

    Removed spaces from file names

--- client/src/lib/tracks.ts
+++ client/src/lib/tracks.ts
@@ -1,4 +1,4 @@
 export const tracks = [
-  { name: 'Eli Brown', url: '/audio/Eli Brown - Be The One.mp3' },
-  { name: 'Dom Dolla', url: '/audio/Dom Dolla - Miracle Maker.mp3' },
+  { name: 'Eli Brown', url: '/audio/eli-brown-be-the-one.mp3' },
+  { name: 'Dom Dolla', url: '/audio/dom-dolla-miracle-maker.mp3' },
 ];
